ff4a7f5f100fdcc3c7018db2c498dc3500abb35e
Timur Karimov <tationkar@gmail.com>
2023-05-28 01:13:11 +0300

    scnd task edit

--- DZ3_Task2/Program.cs
+++ DZ3_Task2/Program.cs
@@ -5,16 +5,16 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53 */
 Console.Clear();
 Console.Write("Введите координату AX: ");
-int ax = int.Parse(Console.ReadLine());
+int ax = int.Parse(Console.ReadLine()!);
 Console.Write("Введите координату AY: ");
-int ay = int.Parse(Console.ReadLine());
+int ay = int.Parse(Console.ReadLine()!);
 Console.Write("Введите координату AZ: ");
-int az = int.Parse(Console.ReadLine());
+int az = int.Parse(Console.ReadLine()!);
 Console.Write("Введите координату BX: ");
-int bx = int.Parse(Console.ReadLine());
+int bx = int.Parse(Console.ReadLine()!);
 Console.Write("Введите координату BY: ");
-int by = int.Parse(Console.ReadLine());
+int by = int.Parse(Console.ReadLine()!);
 Console.Write("Введите координату BZ: ");
-int bz = int.Parse(Console.ReadLine());
+int bz = int.Parse(Console.ReadLine()!);
 double s = Math.Sqrt(Math.Pow((ax-bx), 2) + Math.Pow((ay-by), 2) + Math.Pow((az-bz), 2));
 Console.WriteLine($"Расстояние между точками {Math.Round(s, 2)}");
